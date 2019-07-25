cb47666850a7ee1b786d74c44ee913833f9112c4
Aya <aayooyaa@gmail.com>
2019-07-26 01:59:49 +0300

    final project

---
+++ Maze (Fight your Fear)/Assets/scripts/swordRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordRotation : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("monster"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+   
+}
